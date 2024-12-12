0645a20f9415e10b91a0af68e3048eb679316f1f
Azunn0801 <125119856+Azunn0801@users.noreply.github.com>
2024-12-12 08:26:21 +0700

    Add files via upload

---
+++ BTTH1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    printf("| STT | Ho va Ten       |Toan|Vat Ly| Hoa |Sinh| Van| Diem TB|\n");
+    printf("|----|------------------|----|------|-----|----|----|--------|\n");
+    printf("|  1  | Nguyen Van A    | 7.5|  7.0 | 5.5 | 8.5| 7.5|     7.4|\n");
+    printf("|  2  | Nguyen Van B    | 7.0|  8.5 | 9.0 | 5.0| 6.5|     7.7|\n");
+    printf("|  3  | Nguyen Van C    | 6.5|  7.2 | 6.5 |10.0| 5.5|     7.2|\n");
+
+    return 0;
+}
