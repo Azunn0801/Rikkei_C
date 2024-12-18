bcbec40820a931a4daf40449622e860e4fe90418
Azunn0801 <125119856+Azunn0801@users.noreply.github.com>
2024-12-18 21:11:35 +0700

    Add files via upload

--- B7.c
+++ B7.c
@@ -3,6 +3,19 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int n;
+    printf("nhap so luong mang ");
+    scanf("%d", &n);
+    printf("\nNhap phan tu la so le trong mang: ");
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        while(arr[i] % 2 == 0)
+        {
+            printf("\nPhai nhap so le: ");
+            scanf("%d", &arr[i]);
+        }
+    }
     return 0;
 }
